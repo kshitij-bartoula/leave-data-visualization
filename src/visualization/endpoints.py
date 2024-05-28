cf6c1a1a4e6cf4ae3bab8737b2830d7382cba6c0
kshitij-bartoula <kshitij.bartoula@laudio.com>
2024-05-28 18:04:20 +0545

    self hosting testing

--- src/visualization/endpoints.py
+++ src/visualization/endpoints.py
@@ -8,7 +8,3 @@ ENDPOINT_LEAVE_DISTRIBUTION = f'http://{HOST}:8000/leave_distribution'
 ENDPOINT_LEAVE_TREND_FISCAL_YEAR = f'http://{HOST}:8000/fiscal_year_leave_type_trend'
 ENDPOINT_DEPARTMENT_LEAVE_DISTRIBUTION = f'http://{HOST}:8000/department_leave_distribution'
 ENDPOINT_LEAVE_REASONS = f'http://{HOST}:8000/most_frequent_leave_reason'
-
-
-
-
