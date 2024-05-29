daf17ca6952ba61c5cf5e684730883492397edb8
kshitij-bartoula <kshitij.bartoula@laudio.com>
2024-05-29 12:41:52 +0545

    testing

--- src/uvicorn_run.py
+++ src/uvicorn_run.py
@@ -1,8 +1,10 @@
 import os
 import uvicorn
 
+#port=int(os.getenv("PORT", 8000))
+
 def run_server():
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 if __name__ == "__main__":
     run_server()
