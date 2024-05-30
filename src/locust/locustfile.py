2e1fb1706544e37323de0e016f6121c4d6f7c4a9
kshitij-bartoula <kshitij.bartoula@laudio.com>
2024-05-30 12:51:28 +0545

    imlemented locust testing for load testing

---
+++ src/locust/locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, between, task
+
+class PlotlyDashUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def load_main_page(self):
+        self.client.get("/")
+
+    @task
+    def load_graphs(self):
+        self.client.get("/graphs")
+        self.client.get("/graphs/employee-leave")
+        self.client.get("/graphs/leave-trend")
+        self.client.get("/graphs/leave-distribution")
