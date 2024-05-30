9f52093ff6182e1d6f1eafdd322368bd8f21dee3
kshitij-bartoula <kshitij.bartoula@laudio.com>
2024-05-30 16:11:47 +0545

    testing

--- src/locust/locustfile.py
+++ src/locust/locustfile.py
@@ -1,17 +1,30 @@
-from locust import HttpUser, between, task
+# from locust import HttpUser, between, task
 
-class PlotlyDashUser(HttpUser):
-    wait_time = between(1, 5)
+# class PlotlyDashUser(HttpUser):
+#     wait_time = between(1, 5)
+
+#     @task
+#     def load_main_page(self):
+#         self.client.get("/")
+
+#     @task
+#     def load_graphs(self):
+#         self.client.get("/graphs")
+#         self.client.get("/graphs/employee-leave")
+#         self.client.get("/graphs/leave-trend")
+#         self.client.get("/graphs/leave-distribution")
+
+
+from locust import HttpUser, task, between
+
+
+# Import the plotly Dash app
+from visualization import app
+
+
+class MyUser(HttpUser):
+    wait_time = between(1, 5)  # Time between consecutive requests
 
     @task
     def load_main_page(self):
         self.client.get("/")
-
-    @task
-    def load_graphs(self):
-        self.client.get("/graphs")
-        self.client.get("/graphs/employee-leave")
-        self.client.get("/graphs/leave-trend")
-        self.client.get("/graphs/leave-distribution")
-
-#test
