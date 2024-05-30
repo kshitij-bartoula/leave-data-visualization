680f72d9b6386dcd6a55d387b454244b5406fa90
kshitij-bartoula <kshitij.bartoula@laudio.com>
2024-05-30 17:39:47 +0545

    testing'

--- src/locust/locustfile.py
+++ src/locust/locustfile.py
@@ -5,72 +5,8 @@ class MyUser(HttpUser):
 
     @task
     def load_main_page(self):
-        response = self.client.get("/plotly-dash:8050")
+        response = self.client.get("http://plotly-dash:8050")
         assert response.status_code == 200
-
-    # @task
-    # def test_employee_dropdown(self):
-    #     with self.app.test_client() as client:
-    #         response = client.get("/")
-    #         assert response.status_code == 200
-
-    #         # Verify that the employee dropdown is rendered
-    #         assert b'employee-dropdown' in response.data
-
-    # @task
-    # def test_leave_trend_graph(self):
-    #     with self.app.test_client() as client:
-    #         response = client.get("/")
-    #         assert response.status_code == 200
-
-    #         # Verify that the leave trend graph is rendered
-    #         assert b'leave-trend-graph' in response.data
-
-    # @task
-    # def test_leave_trend_fiscal_year_graph(self):
-    #     with self.app.test_client() as client:
-    #         response = client.get("/")
-    #         assert response.status_code == 200
-
-    #         # Verify that the leave trend (fiscal year) graph is rendered
-    #         assert b'leave-trend-fiscal-year-graph' in response.data
-
-    # @task
-    # def test_employee_leave_graph(self):
-    #     with self.app.test_client() as client:
-    #         response = client.get("/")
-    #         assert response.status_code == 200
-
-    #         # Verify that the employee leave graph is rendered
-    #         assert b'employee-leave-graph' in response.data
-
-    # @task
-    # def test_department_leave_distribution_graph(self):
-    #     with self.app.test_client() as client:
-    #         response = client.get("/")
-    #         assert response.status_code == 200
-
-    #         # Verify that the department leave distribution graph is rendered
-    #         assert b'department-leave-distribution-graph' in response.data
-
-    # @task
-    # def test_leave_reason_graph(self):
-    #     with self.app.test_client() as client:
-    #         response = client.get("/")
-    #         assert response.status_code == 200
-
-    #         # Verify that the leave reason graph is rendered
-    #         assert b'leave-reason-graph' in response.data
-
-    # @task
-    # def test_leave_distribution_graph(self):
-    #     with self.app.test_client() as client:
-    #         response = client.get("/")
-    #         assert response.status_code == 200
-
-    #         # Verify that the leave distribution graph is rendered
-    #         assert b'leave-distribution-graph' in response.data
-
 
 # from locust import HttpUser, task, between
 # import dash
