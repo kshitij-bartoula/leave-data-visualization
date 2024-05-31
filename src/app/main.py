06812c5971282e85cc7fa1e81d79fdafb48438f6
kshitij-bartoula <kshitij.bartoula@laudio.com>
2024-05-31 17:01:30 +0545

    testing

--- src/app/main.py
+++ src/app/main.py
@@ -1,23 +1,23 @@
 """
 Configures FastAPI application and includes API routes.
 """
-# from fastapi import FastAPI
-# from app.scripts.api import router as api_router
-
-# app = FastAPI()
-
-# app.include_router(api_router)
-
 from fastapi import FastAPI
 from app.scripts.api import router as api_router
-from app.scripts.api import router as api_router, startup_event
-import asyncio
 
 app = FastAPI()
 
-# Include the API router
 app.include_router(api_router)
 
-# Register the startup event handler
-app.add_event_handler("startup", startup_event)
+# from fastapi import FastAPI
+# from app.scripts.api import router as api_router
+# from app.scripts.api import router as api_router, startup_event
+# import asyncio
+
+# app = FastAPI()
+
+# # Include the API router
+# app.include_router(api_router)
+
+# # Register the startup event handler
+# app.add_event_handler("startup", startup_event)
 
--- start_up_file.sh
+++ start_up_file.sh
@@ -5,6 +5,13 @@ echo "my cronjob is working!"
 # Run etl
 python /app/etl/etl_main.py
 
+echo "db conection executing!"
+python /app/scripts/database.py
+
+echo "api.py executing!"
+python app/scripts/api.py
+echo "api.py executed!"
+
 #Run dw_tables.py
 #python /app/etl/dw_tables.py
 
