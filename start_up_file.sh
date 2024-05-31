91da07b7bd03e20963d7beea807b27c80d45813b
kshitij-bartoula <kshitij.bartoula@laudio.com>
2024-05-31 15:34:39 +0545

    testing

--- start_up_file.sh
+++ start_up_file.sh
@@ -5,6 +5,10 @@ echo "my cronjob is working!"
 # Run etl
 python /app/etl/etl_main.py
 
+echo "api.py is executing"
+python /app/app/scripts/api.py
+echo "api.py is execution complete"
+
 #Run dw_tables.py
 #python /app/etl/dw_tables.py
 
