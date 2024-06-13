12ebee3c26a7ee5515c3d90186e4d71807bdf7be
kshitij-bartoula <bartoula3@gmail.com>
2024-06-13 09:00:34 +0545

    testing etl

--- start_up_file.sh
+++ start_up_file.sh
@@ -3,10 +3,4 @@
 echo "my cronjob is working!"
 
 # Run etl
-#python /app/etl/etl_main.py
-
-#Run dw_tables.py
-#python /app/etl/dw_tables.py
-
-#Run kpi_views.py
-#python /app/etl/kpi_views.py
+python /app/etl/etl_main.py
