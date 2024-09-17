cb98de62643640ca0ddc9b997dd4aa563b556f2c
kshitij-bartoula <bartoula3@gmail.com>
2024-09-17 22:58:08 +0545

    testing

--- src/db/sql/100_create_raw_schema.sql
+++ src/db/sql/100_create_raw_schema.sql
@@ -3,10 +3,3 @@ BEGIN;
 CREATE SCHEMA  IF NOT EXISTS raw;
 
 COMMIT;
-
-100_create_raw_schema.sql
-101_create_dw_schema.sql
-102_create_dw_employee_details.sql
-103_create_dw_leave_type.sql
-104_create_dw_fiscal_detail.sql
-105_create_dw_allocations.sql
